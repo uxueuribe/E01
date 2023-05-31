39f68f2609ab7f1929636eef7fc202400f8cbff0
Uxue Uribe <uxue.uribe@alumni.mondragon.edu>
2023-05-31 18:44:26 +0200

    este es mi segundo commit

---
+++ codigo.R
@@ -0,0 +1,15 @@
+library(readxl)
+library(dplyr)
+df<-read_xlsx('online_retail_II.xlsx')
+str(df)
+summary(df)
+#Calcula cuántos pedidos distintos hay en el fichero
+hola<-df%>%
+  distinct(Invoice)%>%
+  count()
+#Calcula el precio medio de producto por pedido
+consulta2<-df%>%
+  group_by(Invoice)%>%
+  summarise(total_prod=sum(Quantity), total_prec=sum(Price))
+consulta2$media<-(consulta2$total_prec/consulta2$total_prod)
+
